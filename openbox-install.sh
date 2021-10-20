78e020ae6a58cd09f7d1abe990ee13db8ee020cd
Snehit Sah <snehitsah@protonmail.com>
2021-10-20 10:15:25 +0530

    Update install script
    Signed-off-by: Snehit Sah <snehitsah@protonmail.com>

--- openbox-install.sh
+++ openbox-install.sh
@@ -2,11 +2,12 @@
 
 # Easy setup for testing
 
+mkdir -p ~/.config
+mkdir -p ~/.themes
 cp -R .config/* ~/.config/
 cp -R .themes/* ~/.themes/
-cp .profile ~/.profile
 cp .gtkrc-2.0 ~/.gtkrc-2.0
 chmod -R +x ~/.config/openbox/scripts
-chmod -R +x ~/.config/rofi/scripts
+chmod +x ~/.config/rofi/powermenu.sh
 sudo pacman -Syu --needed --noconfirm - < packages-repository.txt
 dbus-launch dconf load / < xed.dconf
